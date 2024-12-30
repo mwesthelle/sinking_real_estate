4604d80711a09c8f3406d6a7d2f16d3b5f22d216
Matheus Westhelle <matheus.westhelle@gmail.com>
2024-12-29 21:25:09 -0300

    progress on EDA

--- analysis/df_utils.py
+++ analysis/df_utils.py
@@ -1,11 +1,11 @@
 import polars as pl
 
 
-def get_list_column_max_len(df: pl.LazyFrame, list_column: str):
-    return df.select(pl.col(list_column).list.len().max()).collect().item()
+def get_list_column_max_len(df: pl.DataFrame, list_column: str):
+    return df.select(pl.col(list_column).list.len().max()).item()
 
 
-def one_hot_encode_list_column(df: pl.LazyFrame, list_column: str) -> pl.LazyFrame:
+def one_hot_encode_list_column(df: pl.DataFrame, list_column: str) -> pl.DataFrame:
     """
     One-hot encode a column containing lists in a Polars DataFrame.
 
@@ -18,13 +18,13 @@ def one_hot_encode_list_column(df: pl.LazyFrame, list_column: str) -> pl.LazyFra
     """
     # Get unique values across all lists
     unique_values = (
-        df.select(pl.col(list_column).explode()).collect().to_series().unique().sort()
+        df.select(pl.col(list_column).explode()).to_series().unique().sort()
     )
 
     # Creat one-hot encoded columns
     encoded_columns = [
         pl.col(list_column).list.contains(value).alias(f"{list_column}_{value}")
-        for value in unique_values
+        for value in unique_values if value is not None
     ]
 
-    return df.with_columns(encoded_columns)
+    return df.with_columns(encoded_columns).drop(list_column)
